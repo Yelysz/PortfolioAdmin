ca616730a20f6df85af9dcacccf0e04dcf811ca0
Yelysz <anyelystorres17@gmail.com>
2024-05-16 20:45:44 -0400

    database changes

--- src/bd.js
+++ src/bd.js
@@ -1,10 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/portfolio");
-        console.log('>>> DB is connected');
-    } catch(error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log(">>> DB is connected");
+  } catch (error) {
+    console.error("Error connecting to DB:", error);
+    process.exit(1);
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import app from "./app.js";
 import { connectDB } from "./bd.js";
+const PORT = process.env.PORT || 3000;
 
 connectDB();
-app.listen(3000)
-console.log('Server on port', 3000)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
