ca10a81624942fb7daafd5ed051ca84770979e80
Yelysz <anyelystorres17@gmail.com>
2024-05-18 07:22:01 -0400

    cors add

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 import projectRoutes from "./routes/project.routes.js";
 import experienceRoutes from "./routes/experience.routes.js";
 import educationRoutes from "./routes/education.routes.js";
 import skillRoutes from "./routes/skill.routes.js";
 
 const app = express();
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
